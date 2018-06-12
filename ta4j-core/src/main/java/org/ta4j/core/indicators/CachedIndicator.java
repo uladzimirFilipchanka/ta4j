26475160a63e793413cd2257409cc6e9c7b4b9d1
Vladimir Filipchanka <ufilipchanka@fitbit.com>
2018-06-12 10:41:48 +0300

    Cached Indicator: more relazed cleaning

--- ta4j-core/src/main/java/org/ta4j/core/indicators/CachedIndicator.java
+++ ta4j-core/src/main/java/org/ta4j/core/indicators/CachedIndicator.java
@@ -1,18 +1,18 @@
 /**
  * The MIT License (MIT)
- *
+ * <p>
  * Copyright (c) 2014-2017 Marc de Verdelhan & respective authors (see AUTHORS)
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
  * the Software without restriction, including without limitation the rights to
  * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
  * the Software, and to permit persons to whom the Software is furnished to do so,
  * subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
  * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
@@ -25,6 +25,7 @@ package org.ta4j.core.indicators;
 import org.ta4j.core.Indicator;
 import org.ta4j.core.TimeSeries;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -35,6 +36,8 @@ import java.util.List;
  * Caches the constructor of the indicator. Avoid to calculate the same index of the indicator twice.
  */
 public abstract class CachedIndicator<T> extends AbstractIndicator<T> {
+    public static final int ITEMS_TO_REMOVE_THRESHOLD = 100;
+    public static final int ITEMS_TO_REMOVE_DIVIDER = 10;
 
     /** List of cached results */
     private final List<T> results = new ArrayList<T>();
@@ -146,12 +149,10 @@ public abstract class CachedIndicator<T> extends AbstractIndicator<T> {
      */
     private void removeExceedingResults(int maximumResultCount) {
         int resultCount = results.size();
-        if (resultCount > maximumResultCount) {
-            // Removing old results
-            final int nbResultsToRemove = resultCount - maximumResultCount;
-            for (int i = 0; i < nbResultsToRemove; i++) {
-                results.remove(0);
-            }
+        final int nbResultsToRemove = resultCount - maximumResultCount;
+        if (nbResultsToRemove > ITEMS_TO_REMOVE_THRESHOLD &&
+            nbResultsToRemove > maximumResultCount / ITEMS_TO_REMOVE_DIVIDER) {
+            results.subList(0, nbResultsToRemove).clear();
         }
     }
 }
